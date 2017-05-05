d400ace974bbb041d53188dac03936ea098d46df
Caleb Sanders <colonelsanders217@gmail.com>
2017-05-05 16:14:48 -0600

    Unit Test Project created Signed-off-by: Caleb Sanders <colonelsanders217@gmail.com>

---
+++ Multi-Translator/Multi-TranslatorTest/APITest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Multi_TranslatorTest
+{
+    [TestClass]
+    public class APITest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
