4e19211c11c36dad57f5a1fa7ef24ba2cbcc7038
Caleb Sanders <colonelsanders217@gmail.com>
2017-05-08 10:11:37 -0600

    Beginning work on the API Test Signed-off-by: Caleb Sanders <colonelsanders217@gmail.com>

---
+++ Multi-Translator/Multi-Translator/Translator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.IO;
+using Google.Apis.Translate;
+
+namespace Multi_Translator
+{
+    public class Translator
+    {
+       
+    }
+}
